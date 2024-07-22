b23f1d64b554ce00a7207b52c8405e062ac812fb
3bHussein <39864308+3bHussein@users.noreply.github.com>
2024-07-22 23:51:01 +0400

    /

--- 100 Python/1-day.py
+++ 100 Python/1-day.py
@@ -1,14 +1,16 @@
-
-# print('hello')
-
-
 # name=input('enter you name\n')
 # print("hello" +" "+ " hlloo")
 # print(name,"wow you name this ",name)
-
 # print("hello"+input("what is your name\n"))
+# name =input("what is your name \n")
+# length=len(name) 
+# print(length)
 
 
-name =input("what is your name \n")
-length=len(name) 
-print(length)
+# 17. Day 1 Project: Band Name Generator
+print("welcome to my small quiz")
+
+city = input("What's the name of the city  you grow up ?\n")
+pet = input("What's  Your pet's name ?\n")
+
+print("your band name could be "+ city +" "+pet)
