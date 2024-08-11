31b65024c07043a4e0b0b2db7c7c610604818afb
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2024-08-11 08:40:31 +0400

    /

---
+++ 100 Python/4-Day.py
@@ -0,0 +1,97 @@
+
+import My_File
+
+
+
+# a=random.randrange(5,105)
+# print(a)
+
+# b=random.randint(3,53)
+# t=str(a)
+# for x in t:
+#     print(x)
+
+
+
+
+
+# float_nubmer=My_File.random()
+
+
+# p =round(My_File.pi,2)
+
+
+# print(p)
+
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+
+states_of_america.append('hello')
+print(states_of_america)
+
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+
+
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
+
+####### Debugging challenge: #########
+#Try running this code and type 5.
+#It will give you an IndexError and point to line 32 as the issue.
+#But on line 38 we are trying to prevent a crash by detecting
+#any numbers great than or equal to 3 or less than 0.
+#So what's going on?
+#Can you debug the code and fix it?
+#Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
---
+++ 100 Python/5-day.py
@@ -0,0 +1,7 @@
+
+
+d=['a',4,'3']
+
+for x in d :
+    print(x)
+    print(d)
