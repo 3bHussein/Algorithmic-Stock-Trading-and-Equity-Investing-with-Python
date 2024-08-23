8d30074a31512a20acf3618d96cf1145e897ba7f
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2024-08-23 22:18:39 +0400

    /

--- 100 Python/5-day.py
+++ 100 Python/5-day.py
@@ -28,9 +28,32 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+Password=""
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+for char in range(0,nr_letters):
+# for char in range(1,nr_letters,1):
+    
+#   ranChar= random.choice(letters)  
+  Password += random.choice(letters) 
+
+
+for char in range(0,nr_symbols):
+  Password += random.choice(symbols) 
+
+for char in range(0,nr_numbers):
+
+  Password += random.choice(numbers) 
+  
+  
+print(Password)
+
+
+  
+ 
+
+ 
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
