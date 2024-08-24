30d7061dc674961586c122ca705ba2561e84ef1f
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2024-08-24 18:57:23 +0400

    Update 6-Day.py

--- 100 Python/6-Day.py
+++ 100 Python/6-Day.py
@@ -15,38 +15,13 @@ def slove_Hrdle():
     
 
 # slove Hurdle1
-move()
-turn_left()
-move()
-turn_right()
-move()
-turn_right()
-move()
-turn_left()
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
     
+    
+#6 time funcation
 
-def slove_Hrdle():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
-    
-    
-    
-slove_Hrdle()
-slove_Hrdle()
-slove_Hrdle()
-slove_Hrdle()
-slove_Hrdle()
-slove_Hrdle()
+for step in range(6):
+    slove_Hrdle()
+
 
 https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
