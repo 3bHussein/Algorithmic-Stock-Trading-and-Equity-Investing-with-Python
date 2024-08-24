a9bb5af6367291aae3367965dc3420f84299aecc
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2024-08-25 00:42:30 +0400

    /

--- 100 Python/6-Day.py
+++ 100 Python/6-Day.py
@@ -1,27 +1,54 @@
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
     
-def slove_Hrdle():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
+# def slove_Hrdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
     
 
-# slove Hurdle1
+# # slove Hurdle1
 
     
     
-#6 time funcation
+# #6 time funcation
 
-for step in range(6):
-    slove_Hrdle()
+# for step in range(6):
+#     slove_Hrdle()
 
 
-https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+
+
+def print_x(x):
+    print(x)
+    if x =="hello":
+        print('wow you write hello ')
+    else:
+        print("no back and write hello")
+
+
+
+
+print_x("f")
+
+
+
+
+
+
+
+# For iten in list of item 
+    #Do something to each item
+
+# while something is true
+# do something Repeatedly
+
